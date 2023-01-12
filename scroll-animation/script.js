65d41587251de9e43274b65e8c7843a768b09565
xiaolong.wang1 <xiaolong.wang1@tusen.ai>
2023-01-12 16:58:59 +0800

    day06 scroll-animation

--- scroll-animation/script.js
+++ scroll-animation/script.js
@@ -1,9 +1,18 @@
 const boxs =  document.querySelectorAll('.box');
 
-window.addEventListener('scroll',active)
 
+window.addEventListener('scroll',active)
+active()
 function active() {
+
+    const triggerBottom = window.innerHeight / 5 * 4
     boxs.forEach(box => {
-        box.classList.toggle('active');
+        const boxTop = box.getBoundingClientRect().top
+
+        if ( boxTop < triggerBottom ) {
+            box.classList.add('active')
+        }else{
+            box.classList.remove('active')
+        }
     })
 }
